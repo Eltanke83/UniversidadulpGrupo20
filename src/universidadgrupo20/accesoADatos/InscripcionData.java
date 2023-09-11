21ef5f5622cb172b175be9c97c43a967cda5b205
Eltanke83 <141678542+Eltanke83@users.noreply.github.com>
2023-09-11 13:03:23 -0300

    agregacion de clases

--- src/universidadgrupo20/accesoADatos/InscripcionData.java
+++ src/universidadgrupo20/accesoADatos/InscripcionData.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo20.accesoADatos;
 
-/**
- *
- * @author Administrador
- */
+import java.sql.Connection;
+
+
 public class InscripcionData {
+    
+    private Connection con;
+    private MateriaData matData;
+    private AlumnoData aluData;
+    
+    
     
 }
--- src/universidadgrupo20/accesoADatos/MateriaData.java
+++ src/universidadgrupo20/accesoADatos/MateriaData.java
@@ -5,10 +5,33 @@
  */
 package universidadgrupo20.accesoADatos;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Administrador
  */
 class MateriaData {
+    private Connection con=null;
     
-}
+    public MateriaData(){
+    
+    con = Conexion.getConexion();
+    
+    public void guardarMateria(Materia materia){
+        
+        Materia materia=null;
+        
+        String sql= ""
+        
+    
+    
+    }
+    }
+    
+    
+    
+    
+    
+    
+}  
